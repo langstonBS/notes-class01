be4f4d3d2f4bae70f0ad142b1ba791ae94655bb1
langstonBS <langston.beckwithstanley@gmail.com>
2020-06-02 09:40:10 -0700

    all test passed

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const minimist = require("minimist");
 const { parse, valid } = require('./lib/input');
-const { exicute } = require('./lib/notes');
+const { exicute} = require('./lib/notes');
 
+const parsing = parse(process.argv);
+const valadating = valid(parsing);
+const exicuteing = exicute(valadating);
 
 
 
